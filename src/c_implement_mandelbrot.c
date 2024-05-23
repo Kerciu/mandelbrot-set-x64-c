2e412db79129a1f502bb157da12017afb6724aef
Kerciu <kerciuuu@gmail.com>
2024-05-23 18:11:12 +0200

    fixed bugs

--- src/c_implement_mandelbrot.c
+++ src/c_implement_mandelbrot.c
@@ -6,12 +6,12 @@ typedef struct complex {
     double y; // Imaginary portion
 } Complex;
 
-Complex* complexPower(Complex* num, int p) {
+Complex* complexSquared(Complex* num) {
     Complex* newComplex = (Complex*)malloc(sizeof(Complex));
     if (newComplex == NULL) return NULL;
 
-    // x^2 + 2jxy + y^2
-    newComplex->x = pow(num->x, 2) + pow(num->y, 2);
+    // x^2 + 2jxy - y^2
+    newComplex->x = pow(num->x, 2) - pow(num->y, 2);
     newComplex->y = 2 * num->x * num->y;
 
     return newComplex;
@@ -29,24 +29,34 @@ Complex* complexAdd(Complex* a, Complex* b) {
 
 double complexNorm(Complex* num) {
     // Return square magnitude
-
     return sqrt(pow(num->x, 2) + pow(num->y, 2));
 }
 
 int isInMandelbrotSet(Complex* c, int processPower, int setPoint) {
-    Complex* z = (double*)malloc(sizeof(Complex));
+    Complex* z = (Complex*)malloc(sizeof(Complex));
     if (z == NULL) return -1;
 
     z->x = 0;
     z->y = 0;
 
-    // z = z ** 2 + c
+    // z = z ** 2 + c ; Iterate in order to check whether c is in mandelbrot set
     for (int i = 0; i < processPower; ++i) {
-        Complex* temp = complexAdd(complexPower(z, 2), c);
-        free(z);
-        z = temp;
+        Complex* zPowered = complexSquared(z, 2);
+        if (zPowered == NULL)
+        {
+            free(z);
+            return -1;
+        }
+        Complex* zAdded = complexAdd(zPowered, c);
+        free(zPowered);
+        if (zAdded == NULL)
+        {
+            free(z);
+            return -1;
+        }
 
-        if (z == NULL) return -1;
+        free(z);
+        z = zAdded;
 
         // Check if this point is still in the set
         if (complexNorm(z) > setPoint)
