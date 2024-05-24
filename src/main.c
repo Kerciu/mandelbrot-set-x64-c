1fb001d3091ad0631eea03b47e7e59f8a0589a9a
Kerciu <kerciuuu@gmail.com>
2024-05-24 13:16:36 +0200

    added save to bmp func

--- src/main.c
+++ src/main.c
@@ -5,7 +5,49 @@
 #include "mandelbrot.h"
 #include "c_implementation/mandelbrotTools.h"
 
+void saveBMP(const char *filename, int width, int height, unsigned char *buffer) {
+    FILE *f;
+    unsigned char *img = NULL;
+    int filesize = 54 + 3 * width * height;
 
+    img = (unsigned char *)malloc(3 * width * height);
+    if (img == NULL) {
+        printf("Failed to allocate memory for BMP\n");
+        return;
+    }
+
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int x = i;
+            int y = (height - 1) - j;
+            img[(x + y * width) * 3 + 2] = buffer[(i + j * width) * 3 + 0];
+            img[(x + y * width) * 3 + 1] = buffer[(i + j * width) * 3 + 1];
+            img[(x + y * width) * 3 + 0] = buffer[(i + j * width) * 3 + 2];
+        }
+    }
+
+    unsigned char bmpfileheader[14] = {
+        'B', 'M',  filesize & 0xFF, (filesize >> 8) & 0xFF, (filesize >> 16) & 0xFF, (filesize >> 24) & 0xFF,
+        0, 0, 0, 0, 54, 0, 0, 0
+    };
+    unsigned char bmpinfoheader[40] = {
+        40, 0, 0, 0,  width & 0xFF, (width >> 8) & 0xFF, (width >> 16) & 0xFF, (width >> 24) & 0xFF,
+        height & 0xFF, (height >> 8) & 0xFF, (height >> 16) & 0xFF, (height >> 24) & 0xFF,
+        1, 0, 24, 0
+    };
+
+    f = fopen(filename, "wb");
+    if (f == NULL) {
+        printf("Failed to open BMP file\n");
+        free(img);
+        return;
+    }
+    fwrite(bmpfileheader, 1, 14, f);
+    fwrite(bmpinfoheader, 1, 40, f);
+    fwrite(img, 1, 3 * width * height, f);
+    fclose(f);
+    free(img);
+}
 
 int main() {
     glfwInit();
@@ -30,23 +72,14 @@ int main() {
     // Initialize mandelbrot set
     int width = 800;
     int height = 600;
-    int* array = (int*)malloc(width * height * sizeof(int));
-    if (array == NULL) {
-        printf("Failed to allocate memory for Mandelbrot array\n");
+    unsigned char* buf = (unsigned char*)malloc(width * height * 3 * sizeof(unsigned char));
+    if (buf == NULL) {
+        printf("Failed to allocate memory for pixel buffer\n");
         glfwTerminate();
         return -1;
     }
-    mandelbrotIterationTable(array, width, height, -2.0, 2.0, -2.0, 2.0, 25, 10);
-
-    int size = width * height;
-    for (int i = 0; i < size; i++) {
-        if (array[i] == 0) {
-            // Black pixel
-        }
-        else {
-            // other pixel
-        }
-    }
+    createMandelbrot(buf, width, height, -2.0, 2.0, -2.0, 2.0, 25, 10);
+    saveBMP("mandelbrot.bmp", width, height, buf);
 
 
     while (!glfwWindowShouldClose(window)) {
@@ -57,10 +90,12 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // TODO draw mandelbrot here
+        glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, buf);
 
         glfwSwapBuffers(window);
     }
 
+    free(buf);
     glfwTerminate();
     return 0;
 }
