6a8179847a6b7d4e1db03b7fa4d7cd4d8e86f16e
Kerciu <kerciuuu@gmail.com>
2024-05-26 11:37:34 +0200

    extended prologue and epilogue, added comments cueing each register w/ its argument

--- mandelbrot.s
+++ mandelbrot.s
@@ -1,15 +1,33 @@
 section .text
 global mandelbrot
 
+; int mandelbrot(unsigned char* pixelBuffer, int width, int height, double cReal, double cImag,
+;                int processPower, int setPoint, double centerReal, double centerImag, double zoom);
+;
+; RDI = pixelBuffer;
+; RSI = width;
+; RDX = height
+; RCX = processPower;
+; R8 = setPoint;
+; XMM0 = cReal;
+; XMM1 = cImag;
+; XMM2 = centerReal;
+; XMM3 = centerImag;
+; XMM4 = zoom;
+
 mandelbrot:
     ; prologue
     push rbp
     mov rbp, rsp
+    push rbx
+    push r12
 
     ; body
 
     ; epilogue
 end:
+    pop r12
+    pop rbx
     mov rsp, rbp
     pop rbp
     ret
