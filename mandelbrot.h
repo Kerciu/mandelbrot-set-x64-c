6268909d4848e66b55523282b47a82cda560d3e1
Kerciu <kerciuuu@gmail.com>
2024-05-26 11:36:49 +0200

    added arguments in asm header file

--- mandelbrot.h
+++ mandelbrot.h
@@ -1,6 +1,7 @@
 #ifndef MANDELBROT_SET
 #define MANDELBROT_SET
 
-int mandelbrot(char* s);
+int mandelbrot(unsigned char* pixelBuffer, int width, int height, double cReal, double cImag,
+               int processPower, int setPoint, double centerReal, double centerImag, double zoom);
 
 #endif
