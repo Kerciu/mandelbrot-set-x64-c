8d1b534b44e99b5d9e08c14d20a29573735163ef
Kerciu <Kerciuuu@gmail.com>
2024-05-31 15:12:17 +0200

    Changed main

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
+/*
+===============================================================================
+                            Mandelbrot Set Generator
+===============================================================================
+Author: Kacper Górski
+
+This program generates the Mandelbrot set visualization using SDL2 library.
+It allows customization of the image dimensions, process power, set point,
+and output file name. If no arguments are provided, default values will be used.
+
+Usage: ./mandelbrot [width] [height] [process power] [set point] [output file name]
+If no arguments are provided, default values will be used.
+
+Default values:
+- Width: 800
+- Height: 600
+- Process Power: 25
+- Set Point: 10
+- Output File Name: mandelbrot.bmp
+===============================================================================
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
 #include <SDL2/SDL.h>
 #include "mandelbrot.h"
-// #include "mandelbrotTools.h"
+
+/*
+===============================================================================
+                               Save to BMP File
+===============================================================================
+*/
 
 void saveBMP(const char *filename, int width, int height, unsigned char *buffer) {
     FILE *f;
@@ -51,19 +78,70 @@ void saveBMP(const char *filename, int width, int height, unsigned char *buffer)
     free(img);
 }
 
+/*
+===============================================================================
+                                    Main
+===============================================================================
+*/
+
 int main(int argc, char* argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
+    // Default values
     long WIDTH = 800;
     long HEIGHT = 600;
+    long processPower = 25;
+    long setPoint = 10;
+    const char* outputFilename = "mandelbrot.bmp";
+
+    // Display help message if required
+    if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+        printf("Usage: ./mandelbrot [width] [height] [process power] [set point] [output file name]\n");
+        printf("If no arguments are provided, default values will be used:\n");
+        printf("Width: %ld, Height: %ld, Process Power: %ld, Set Point: %ld, Output-File Name: %s\n",
+                WIDTH, HEIGHT, processPower, setPoint, outputFilename);
+        return 0;
+    }
+
+    // Parse command line arguments
+    if (argc > 1) {
+        WIDTH = strtol(argv[1], NULL, 10);
+    }
+    if (argc > 2) {
+        HEIGHT = strtol(argv[2], NULL, 10);
+    }
+    if (argc > 3) {
+        processPower = strtol(argv[3], NULL, 10);
+    }
+    if (argc > 4) {
+        setPoint = strtol(argv[4], NULL, 10);
+    }
+    if (argc > 5) {
+        outputFilename = argv[5];
+    }
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL initialization failed: %s\n", SDL_GetError());
+        return -1;
+    }
 
     // Window creation
     SDL_Window *window = SDL_CreateWindow("Mandelbrot Set", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if (window == NULL) {
+        printf("Failed to create SDL window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Failed to create SDL renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-    // Mandelbrot buffer
-    uint8_t* buf = (uint8_t*)malloc(WIDTH * HEIGHT * 4 * sizeof(uint8_t*));  // RGBA buf
+    // Mandelbrot RGBA buffer
+    uint8_t* buf = (uint8_t*)malloc(WIDTH * HEIGHT * 4 * sizeof(uint8_t*));
     if (buf == NULL) {
         printf("Failed to allocate memory for pixel buffer\n");
         return -1;
@@ -72,12 +150,12 @@ int main(int argc, char* argv[])
     double centerReal = -0.5;
     double centerImag = 0.0;
     double zoom = 1.0;
-    long processPower = 25;
-    long setPoint = 10;
 
+    // Generate Mandelbrot set
     mandelbrot(buf, WIDTH, HEIGHT, processPower, setPoint, centerReal, centerImag, zoom);
-    // createMandelbrotAssemblified(buf, WIDTH, HEIGHT, processPower, setPoint, centerReal, centerImag, zoom);
-    saveBMP("mandelbrot.bmp", WIDTH, HEIGHT, buf);
+
+    // Save to BMP file if output file name provided
+    if (argc > 5) saveBMP("mandelbrot.bmp", WIDTH, HEIGHT, buf);
 
     SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(buf, WIDTH, HEIGHT, 32, WIDTH * 4, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -95,22 +173,24 @@ int main(int argc, char* argv[])
             }
             else if (e.type == SDL_MOUSEWHEEL)
             {
-                int mouseX = e.wheel.x;
-                int mouseY = e.wheel.y;
+                int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY);
-                double mouseRe = ((double)mouseX - WIDTH / 2.0) * 4.0 / (WIDTH * zoom) + centerReal;
-                double mouseIm = ((double)mouseY - HEIGHT / 2.0) * 4.0 / (HEIGHT * zoom) + centerImag;
+                double beforeZoomRe = ((double)mouseX - WIDTH / 2.0) * 4.0 / (WIDTH * zoom) + centerReal;
+                double beforeZoomIm = ((double)mouseY - HEIGHT / 2.0) * 4.0 / (HEIGHT * zoom) + centerImag;
 
                 if (e.wheel.y > 0)
                 {
-                    zoom *= 1.3;
+                    zoom *= 1.1;
                 }
                 else if (e.wheel.y < 0)
                 {
-                    zoom /= 1.3;
+                    zoom /= 1.1;
                 }
-                centerReal = mouseRe + (centerReal - mouseRe) / 1.1;
-                centerImag = mouseIm + (centerImag - mouseIm) / 1.1;
+                double afterZoomRe = ((double)mouseX - WIDTH / 2.0) * 4.0 / (WIDTH * zoom) + centerReal;
+                double afterZoomIm = ((double)mouseY - HEIGHT / 2.0) * 4.0 / (HEIGHT * zoom) + centerImag;
+
+                centerReal += (beforeZoomRe - afterZoomRe);
+                centerImag += (beforeZoomIm - afterZoomIm);
                 needRedraw = 1;
             }
             else if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT)
@@ -126,7 +206,6 @@ int main(int argc, char* argv[])
         if (needRedraw)
         {
             mandelbrot(buf, WIDTH, HEIGHT, processPower, setPoint, centerReal, centerImag, zoom);
-            // createMandelbrotAssemblified(buf, WIDTH, HEIGHT, processPower, setPoint, centerReal, centerImag, zoom);
             SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(buf, WIDTH, HEIGHT, 32, WIDTH * 4, 0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000);
             SDL_DestroyTexture(texture);
             texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -143,6 +222,7 @@ int main(int argc, char* argv[])
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-
     return 0;
 }
+
+// ============================================================================
